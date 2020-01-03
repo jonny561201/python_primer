d893003684ee7b8e6a7ee872dfba11f5eae332bd
Jon Graf <jongraf1@gmail.com>
2020-01-03 09:16:42 -0600

    updated dependency install to get past the proxy

--- enableEnv.sh
+++ enableEnv.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
-export HTTP_PROXY=http://{user}:{pass}@pfgproxy.principal.com:80
-export HTTPS_PROXY=http://{user}:{pass}@pfgproxy.principal.com:80
 
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org virtualenv
+WHITE='\033[0m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+
+
+echo -e "${GREEN}Enter password for $USERNAME.${WHITE}"
+
+read -s tempPass
+if [ -z "$tempPass" ]
+then
+    exit 0
+fi
+
+encodedPass=$(echo -ne $tempPass | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g')
+
+export HTTP_PROXY=http://${USERNAME}:${encodedPass}@pfgproxy.principal.com:80
+export HTTPS_PROXY=http://${USERNAME}:${encodedPass}@pfgproxy.principal.com:80
+
+#upgrade pip
+echo -e "${YELLOW}-----------Upgrading Python Pip-----------${WHITE}"
+python -m pip install --proxy=$HTTPS_PROXY --trusted-host pypi.org --trusted-host files.pythonhosted.org --trusted-host pypi.python.org requests  --upgrade pip
+
+echo -e "${GREEN}Enter name of dependency to install.${WHITE}"
+
+read -s installDependency
+if [ -z "$installDependency" ]
+then
+    exit 0
+fi
+
+#install dependency
+echo -e "${YELLOW}-----------Installing Dependency-----------${WHITE}"
+python -m pip install --proxy=$HTTPS_PROXY --trusted-host pypi.org --trusted-host files.pythonhosted.org --trusted-host pypi.python.org $installDependency
