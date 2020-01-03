0bb14d0c8a2efdb9d699903f74e3bb5a263a3dfa
Jon Graf <jongraf1@gmail.com>
2020-01-03 12:46:45 -0600

    will prompt user if they want to uninstall python 2 and install python 3

--- installDependencies.sh
+++ installDependencies.sh
@@ -3,16 +3,47 @@
 WHITE='\033[0m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+RED='\033[0;31m'
 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DEV_TOOLS_DIR=/R/DevTools/Scripts
+
+
+function uninstallPython2 {
+    echo -e "${YELLOW}-----------uninstalling Python 2-----------${WHITE}"
+    pushd ${CURRENT_DIR}
+    cd ${DEV_TOOLS_DIR}
+    cmd //c powershell -executionPolicy bypass /R/DevTools/Scripts/Python27-Uninstall.ps1
+    popd
+}
+
+
+function installPython3 {
+    echo -e "${YELLOW}-----------installing Python 3-----------${WHITE}"
+    pushd ${CURRENT_DIR}
+    cd ${DEV_TOOLS_DIR}
+    cmd //c powershell -executionPolicy bypass /R/DevTools/Scripts/Python37-Install.ps1
+    popd
+}
 
 
 function checkPythonVersion {
     PYTHON_VERSION=$(python --version)
-    if [[ $PYTHON_VERSION == "Python 3"* ]] ;
+    if [[ $PYTHON_VERSION == "Python 2"* ]]
     then
         echo -e "${GREEN}Python 3 installed!${WHITE}"
+    elif [[ $PYTHON_VERSION == "Python 3"* ]]
+    then
+        echo -e "${RED}Python 2 installed. ${WHITE}Do you want to uninstall Python 2 and install Python 3(y/n)?"
+        read UNINSTALL_ANSWER
+        if [[ $UNINSTALL_ANSWER == "y" ]]
+        then
+            uninstallPython2
+            installPython3
+        fi
     fi
 }
+
 
 function setupProxyVariables {
     echo -e "Enter password for ${GREEN}$USERNAME:${WHITE}"
@@ -34,6 +65,7 @@ function upgradePip {
     python -m pip install --proxy=$HTTPS_PROXY --trusted-host pypi.org --trusted-host files.pythonhosted.org --trusted-host pypi.python.org requests  --upgrade pip
 }
 
+
 function installDependencies {
     echo -e "\nEnter ${GREEN}name of dependency${WHITE} to install:"
 
@@ -46,6 +78,7 @@ function installDependencies {
     echo -e "${YELLOW}-----------Installing Dependency-----------${WHITE}"
     python -m pip install --proxy=$HTTPS_PROXY --trusted-host pypi.org --trusted-host files.pythonhosted.org --trusted-host pypi.python.org $INSTALL_DEPENDENCY
 }
+
 
 checkPythonVersion
 setupProxyVariables
