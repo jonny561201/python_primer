2e9a37984cad52a285394ed5958eecc336d3e5bf
Jon Graf <jongraf1@gmail.com>
2020-01-09 13:25:53 -0600

    removed python validation components to separate file

--- installDependencies.sh
+++ installDependencies.sh
@@ -5,50 +5,7 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DEV_TOOLS_DIR=/R/DevTools/Scripts
 PROXY_SERVER="stupid.stupid.com:80"
-
-
-function uninstallPython2 {
-    echo -e "${YELLOW}-----------uninstalling Python 2-----------${WHITE}"
-    pushd ${CURRENT_DIR}
-    cd ${DEV_TOOLS_DIR}
-    cmd //c powershell -executionPolicy bypass /R/DevTools/Scripts/Python27-Uninstall.ps1
-    popd
-}
-
-
-function installPython3 {
-    echo -e "${YELLOW}-----------installing Python 3-----------${WHITE}"
-    pushd ${CURRENT_DIR}
-    cd ${DEV_TOOLS_DIR}
-    cmd //c powershell -executionPolicy bypass /R/DevTools/Scripts/Python37-Install.ps1
-    popd
-}
-
-
-function checkPythonVersion {
-    PYTHON_VERSION=$(python --version 2>/dev/null)
-    if [[ $PYTHON_VERSION == "Python 3"* ]] ; then
-        echo -e "${GREEN}Python 3 installed!${WHITE}"
-    elif [[ $PYTHON_VERSION == "Python 2"* ]] ; then
-        echo -e "${RED}Python 2 installed. ${WHITE}Do you want to uninstall Python 2 and install Python 3 (y/n)?"
-        read UNINSTALL_ANSWER
-        if [[ $UNINSTALL_ANSWER == "y" ]] ; then
-            uninstallPython2
-            installPython3
-        fi
-    else
-        echo -e "${YELLOW}Do you want to install Python 3 (y/n)${WHITE}?"
-        read INSTALL_PYTHON
-        if [[ $INSTALL_PYTHON == "y" ]] ; then
-            installPython3
-        else
-            exit 0
-        fi
-    fi
-}
 
 
 function setupProxyVariables {
@@ -98,7 +55,6 @@ function installDependencies {
 }
 
 
-checkPythonVersion
 setupProxyVariables
 upgradePip
 installDependencies
