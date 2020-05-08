a442c84f3a150aa9017d6013388ab963ab8fd63f
Jon <jongraf1@gmail.com>
2020-05-08 15:16:51 -0500

    added some try catch logic to the endpoint

--- svc/routes/fizz_buzz_routes.py
+++ svc/routes/fizz_buzz_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, Response
 
 from svc.utilities.fizz_buzz import fizz_buzz
 
@@ -8,5 +8,10 @@ FIZZ_BUZZ_BLUEPRINT = Blueprint('fizz_buzz', __name__)
 
 @FIZZ_BUZZ_BLUEPRINT.route('/fizzBuzz/<num>')
 def app_test(num):
-    results = fizz_buzz(int(num))
-    return results
+    try:
+        number = int(num)
+        results = fizz_buzz(number)
+        return Response(results, status=200)
+    except ValueError as ex:
+        print(ex)
+        return Response(ex, status=400)
